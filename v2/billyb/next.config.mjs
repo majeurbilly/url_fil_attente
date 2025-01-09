04c3837550b6e80d8c8dbb57923321a4c822d589
majeurbilly <billy.halle.bh@gmail.com>
2025-01-09 17:36:55 -0500

    modif précise au niveau de url front et url dans la config back

--- v2/billyb/next.config.mjs
+++ v2/billyb/next.config.mjs
@@ -1,18 +1,21 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
-        return [{ // correspondance de toutes les routes API
-            source: "/api/list",
-            headers: [
-                {key: "Access-Control-Allow-Credentials", value: "true"}, {
-                    key: "Access-Control-Allow-Origin", value: "https://10.100.2.130:3000/web/"
-                }, {
-                    key: "Access-Control-Allow-Methods", value: "GET,DELETE,POST,OPTIONS"
-                }, {
-                    key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin, X-Requested-With, Content-Type, Accept, Authorization"
-                },]
-        }]
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "http://10.100.2.130:8080/web/" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
     }
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
