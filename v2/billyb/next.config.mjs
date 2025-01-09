afd7fc7e475f72c98c71939ff27814c15d563870
majeurbilly <billy.halle.bh@gmail.com>
2025-01-09 18:09:04 -0500

    original next.config.mjs

--- v2/billyb/next.config.mjs
+++ v2/billyb/next.config.mjs
@@ -1,21 +1,27 @@
-// next.config.js
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://10.100.2.130:8080/web/" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
+        return [{
+            // This will match all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                {
+                    key: "Access-Control-Allow-Origin",
+                    value: "http://10.100.2.130:8080/web/"
+                },
+                {
+                    key: "Access-Control-Allow-Methods",
+                    value: "GET,DELETE,POST,OPTIONS"
+                },
+                {
+                    key: "Access-Control-Allow-Headers",
+                    value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin, X-Requested-With, Content-Type, Accept, Authorization"
+                },
+            ]
+        }]
     }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
